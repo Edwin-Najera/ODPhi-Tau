4cd1c41feef129f834789d750301444c5ec55a5e
Edwin-Najera <leonajera0930@gmail.com>
2026-05-14 03:25:23 -0500

    feature: added github to footer

--- Notorious Tau/src/components/Home/Footer.tsx
+++ Notorious Tau/src/components/Home/Footer.tsx
@@ -1,10 +1,15 @@
+import { FaGithub } from "react-icons/fa";
 import "../global.css";
 
 function Footer() {
   return (
     <div className="footer-container">
       <span className="footer-right">Developed By Edwin Najera</span>
-      <span className="footer-left">Some words</span>
+      <span className="footer-left">
+        <a href="https://github.com/Edwin-Najera" target="_blank">
+          <FaGithub />
+        </a>
+      </span>
     </div>
   );
 }
